5fb59ef35d58f8394f66e3b3cfd6141b490356a8
finebrain <iurii.kyian@insights.md>
2013-12-07 22:00:34 +0100

    added android platform files

---
+++ platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/finebrain/sandbox/phonegap-workspace/lang-flashcards/platforms/android/bin/AndroidManifest.xml : \
+/Users/finebrain/sandbox/phonegap-workspace/lang-flashcards/platforms/android/AndroidManifest.xml \
